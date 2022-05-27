06289d83d3a144e8b74a6252b49dbf90f11ebe80
Pablo Rios <pablo.rios@lobocom.es>
2022-05-27 13:59:05 +0200

    Added image comment to implement logo in home

--- pages/Home.js
+++ pages/Home.js
@@ -1,14 +1,33 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
+import { MotiView } from "moti";
 
 export const Home = () => {
   return (
     <View style={styles.container}>
-      <View style={styles.titleView}>
+      <MotiView
+        from={{ scale: 0, translateY: -100 }}
+        animate={{ scale: 1, translateY: 0 }}
+        exit={{ opacity: 0 }}
+        delay={500}
+        style={styles.titleView}
+      >
+        {/* <Image
+          source={{
+            uri: "https://pbs.twimg.com/profile_images/486929358120964097/gNLINY67_400x400.png",
+          }}
+          style={styles.logo}
+        /> */}
         <Text style={styles.titleText}>HuntZ</Text>
-      </View>
-      <View style={styles.content}>
-        <Text>Bienvenido a HuntZ</Text>
-      </View>
+      </MotiView>
+      <MotiView
+        from={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        delay={1000}
+        style={styles.content}
+      >
+        <Text>Game download already available !</Text>
+      </MotiView>
     </View>
   );
 };
@@ -16,7 +35,7 @@ export const Home = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#e8e8e8",
+    // backgroundColor: "#e8e8e8",
   },
   titleView: {
     flex: 1,
@@ -31,5 +50,10 @@ const styles = StyleSheet.create({
   content: {
     flex: 1,
     alignItems: "center",
+  },
+  logo: {
+    height: 100,
+    width: 100,
+    resizeMode: "contain",
   },
 });
