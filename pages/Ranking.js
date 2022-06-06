6d4a1db0ad1febfc6e06774828f6d8394260045b
Pablo Rios <pablo.rios@lobocom.es>
2022-06-06 14:27:40 +0200

    Added refresh control to ranking flatlist

--- pages/Ranking.js
+++ pages/Ranking.js
@@ -1,5 +1,11 @@
-import { SafeAreaView, FlatList, StatusBar, View } from "react-native";
-import { useState, useEffect } from "react";
+import {
+  SafeAreaView,
+  FlatList,
+  StatusBar,
+  View,
+  RefreshControl,
+} from "react-native";
+import { useState, useEffect, useCallback } from "react";
 import { db } from "../services/firebase";
 import { ref, onValue } from "firebase/database";
 
@@ -12,10 +18,21 @@ import {
 export const Ranking = () => {
   const [loading, setLoading] = useState(true);
   const [data, setData] = useState([]);
+  const [refreshing, setRefreshing] = useState(false);
   const rankingRef = ref(db, "Jugadores");
 
-  useEffect(() => {
+  const wait = (timeout) => {
+    return new Promise((resolve) => setTimeout(resolve, timeout));
+  };
+
+  const onRefresh = useCallback(() => {
+    setRefreshing(true);
+    wait(1000).then(() => setRefreshing(false));
+  }, []);
+
+  const getData = useCallback(async () => {
     let datos = [];
+    setData([]);
 
     onValue(rankingRef, (snapshot) => {
       setLoading(true);
@@ -31,6 +48,10 @@ export const Ranking = () => {
     });
   }, []);
 
+  useEffect(() => {
+    getData();
+  }, []);
+
   const renderItem = ({ item }) => <Item name={item.name} kills={item.kills} />;
 
   return (
@@ -40,12 +61,17 @@ export const Ranking = () => {
         <SafeAreaView
           style={{ flex: 1, marginTop: StatusBar.currentHeight || 0 }}
         >
-          <FlatList
-            ListHeaderComponent={() => <View style={{ height: 10 }} />}
-            data={data}
-            renderItem={renderItem}
-            keyExtractor={(item, id) => id}
-          />
+          {data.length > 1 && (
+            <FlatList
+              refreshControl={
+                <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+              }
+              ListHeaderComponent={() => <View style={{ height: 10 }} />}
+              data={data}
+              renderItem={renderItem}
+              keyExtractor={(item, id) => id}
+            />
+          )}
         </SafeAreaView>
       ) : (
         <LoadingIndicator />
